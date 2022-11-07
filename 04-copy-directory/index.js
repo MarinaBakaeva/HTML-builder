09d898742cf12a1c2a39879b19406049f671722f
MarinaBakaeva <105422227+MarinaBakaeva@users.noreply.github.com>
2022-11-07 18:18:39 +0300

    feat: create script HTML-builder 4
    feat: create script HTML-builder 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs')
+const path = require('path')
 
+const copyDir = () => {
+  fs.mkdir(path.join(__dirname, './files-copy'), {recursive: true}, () => {
+    fs.promises.readdir(path.join(__dirname, './files-copy'))
+    .then(files => files.forEach(el => {
+      fs.unlink(path.join(__dirname,'./files-copy/', el), err => {
+        if(err) return console.error(err)
+      })
+    }))
+    .catch((err) => console.error(err))
+    
+    fs.promises.readdir(path.join(__dirname, './files'), {withFileTypes: true})
+    .then(files => files.forEach(el => {
+      if(el.isFile()) {
+        fs.promises.copyFile(path.join(__dirname, './files', el.name), path.join(__dirname, './files-copy', el.name))
+      }
+    }))
+    .catch((err) => console.error(err))
+  })
+}
+
+copyDir()
